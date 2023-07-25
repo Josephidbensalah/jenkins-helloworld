545e2d925166646eb4fdc52f2163a0a7e824cfd3
Youssef IDBENSALAH <60150631+Josephidbensalah@users.noreply.github.com>
2023-07-25 21:45:03 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, Joseph!");
+        System.out.println("Hello Jenkins ,from Joseph!");
     }
 }
